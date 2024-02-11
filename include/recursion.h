9333e48f032a38e3829b58eefb0cc75ed642b47d
10XTMY <123916890+10XTMY@users.noreply.github.com>
2024-02-11 14:20:25 +0000

    removed redundant get_int declaration in recursion.h

--- include/recursion.h
+++ include/recursion.h
@@ -7,12 +7,4 @@
 
 void draw(int n);
 
-/**
- * Prompts user for a line of text from standard input and returns the
- * equivalent int; if text does not represent an int in [-2^31, 2^31 - 1)
- * or would cause underflow or overflow, user is prompted to retry. If line
- * can't be read, returns INT_MAX.
- */
-int get_int(const char *format, ...) __attribute__((format(printf, 1, 2)));
-
 #endif //PRACTICE_RECURSION_H
